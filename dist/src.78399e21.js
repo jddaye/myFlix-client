9506d811f908256d8580e029a2f67f19d8eb2146
Jessica Daye <jddaye@hotmail.com>
2022-01-18 16:13:39 -0500

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -51566,7 +51566,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52756" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61991" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
